74f6eef593c4eb62bc9f54b8e2e1e8aa487339d7
MackoDev <mackoo.oklinski@gmail.com>
2022-07-10 13:40:17 +0200

    connectionDB

--- src/main/java/com/example/article/adapters/B.java
+++ src/main/java/com/example/article/adapters/B.java
@@ -1,4 +1,14 @@
 package com.example.article.adapters;
 
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Service
 public class B {
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello maciek";
+    }
 }
